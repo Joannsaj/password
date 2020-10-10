3b98e3bede0856e255339c9204c3be212ba831e2
joansaj <joannsaj@gmail.com>
2020-10-10 23:20:38 +0300

    method to delete function

--- user_test.py
+++ user_test.py
@@ -29,10 +29,17 @@ class TestUser(unittest.TestCase):
     def test_save_user(self):
         '''
         test_save_user test case to test if the user object is saved into
-         the user list
+        the user list
         '''
         self.new_user.save_user() # saving the new user
         self.assertEqual(len(User.user_list),1)
+
+    
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            User.user_list = []
 
 
     def test_delete_user(self):
