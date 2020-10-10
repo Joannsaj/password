17f30751f53de8076fb035cae238e11f073906b9
joansaj <joannsaj@gmail.com>
2020-10-10 16:37:54 +0300

    test for set up

--- user_test.py
+++ user_test.py
@@ -9,3 +9,22 @@ class TestUser(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("heyoo","qwaszxer") # create contact object
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.username,"heyoo")
+        self.assertEqual(self.new_user.password,"qwaszxer")
+
+
+if __name__ == '__main__':
+    unittest.main()
